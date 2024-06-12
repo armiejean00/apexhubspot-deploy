69bbb27d4182a99e869af8e589fb257b83801900
armiejean <101634903+armiejean00@users.noreply.github.com>
2024-06-12 16:50:52 +0800

    Update layout.blade.php

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,7 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="{{ asset('assets/img/favicon.png') }}">
     <title>ApexHubSpot</title>
-    @vite(['resources/css/app.css', 'resources/css/main/default.css', 'resources/css/main/sidebar.css', ...$cssPaths])
+      <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+     <link rel="stylesheet" href="{{ asset('css/default.css') }}">
+     <link rel="stylesheet" href="{{ asset('css/sidebar.css') }}">
+
     <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
         integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
