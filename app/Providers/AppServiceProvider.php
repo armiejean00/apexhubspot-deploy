45618f4cc34cded3808e448e4369aa6553e961fe
Armie <armiejeanmiranda@student.laverdad.edu.ph>
2024-06-12 14:55:22 +0800

    addd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\URL;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (config('app.env') != 'local') {
-            \URL::forceScheme('http');
+            URL::forceScheme('http');
         }
     }
 }
